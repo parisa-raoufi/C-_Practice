5cb3250db770841c67340b534bb22616d34b5276
parisa-raoufi <90569583+parisa-raoufi@users.noreply.github.com>
2023-02-09 10:52:12 +0330

    add 2 practices

---
+++ practice1/practice1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace practice1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber= 0;
+            int secondNumber = 1;
+            int thirdNumber;
+            for (int i =0; i <= 100; i++)
+            {
+                thirdNumber = firstNumber + secondNumber;
+                firstNumber = secondNumber;
+                secondNumber = thirdNumber;
+                Console.WriteLine(thirdNumber);
+            }
+        }
+    }
+}
---
+++ practice2/practice2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace practice2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            static void MyMethod()
+            {
+                string[] firstName = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " };
+                string[] lastName = { " ", " ", " ", " ", " ", " ", " ", " ", " ", " " }; ;
+                for (int i =0; i <= 10; i++)
+                {
+                    Console.WriteLine("enter firstName");
+                    firstName[i] = Console.ReadLine();
+                    Console.WriteLine("enter lastName");
+                    lastName[i] = Console.ReadLine();
+                    Console.WriteLine(" ");
+
+                    for (int x = 0; x <= i; x++)
+                    {
+                    MyMethod2( x , firstName[x], lastName[x]);
+                    }
+                    Console.WriteLine(" ");
+
+                }
+
+            }
+
+            static void MyMethod2( int x, string firstName , string lastName)
+            {              
+                Console.WriteLine( x + "_ " + firstName + " " + lastName);
+            }
+            MyMethod();
+        }
+    }
+}
